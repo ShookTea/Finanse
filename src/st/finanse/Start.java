e84d1a6ee4a1b08c4df5b299b02061aac71ee312
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-01 22:30:18 +0100

    Wielkie sprzątanie kodu

--- src/st/finanse/Start.java
+++ src/st/finanse/Start.java
@@ -1,70 +1,25 @@
 package st.finanse;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.UIManager;
-import st.finanse.gui.Frame;
-import st.finanse.proj.Project;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-/**
- *
- * @author ShookTea
- */
-public class Start implements st.init.StartI {
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.io.File;
+
+public class Start extends Application {
+    public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        if (args.length > 0) {
+            toOpen = new File(args[0]);
+        }
+        launch(args);
+    }
+
     @Override
-    public void start(String[] args) {
-        if (System.getProperty("os.name").startsWith("Windows") && (args.length == 0 || !args[0].equals("-t"))) {
-            try {
-                String java = System.getProperty("java.home");
-                String jar = Start.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-                Runtime r = Runtime.getRuntime();
-                java = java.replace("\\", "\\\\") + "\\\\bin\\\\javaw.exe";
-                while (jar.startsWith("\\") || jar.startsWith("/")) {
-                    jar = jar.substring(1);
-                }
-                jar = jar.replace("\\", "\\\\").replace("/", "\\");
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        if (args.length == 1 && !args[0].equals("-t")) {
-            File f = new File(args[0]);
-            Project.load(f);
-        }
-        initUI();
-        Frame.frame.setVisible(true);
+    public void start(Stage primaryStage) throws Exception {
+
     }
-    
-    private static void initUI() {
-        UIManager.put("FileChooser.openDialogTitleText", "Otwórz");
-        UIManager.put("FileChooser.lookInLabelText", "Szukaj w");
-        UIManager.put("FileChooser.openButtonText", "Otwórz");
-        UIManager.put("FileChooser.saveButtonText", "Zapisz");
-        UIManager.put("FileChooser.cancelButtonText", "Anuluj");
-        UIManager.put("FileChooser.fileNameLabelText", "Nazwa pliku");
-        UIManager.put("FileChooser.filesOfTypeLabelText", "Typy plików");
-        UIManager.put("FileChooser.openButtonToolTipText", "Otwórz wybrany plik");
-        UIManager.put("FileChooser.saveButtonToolTipText", "Zapisz w wybranym pliku");
-        UIManager.put("FileChooser.cancelButtonToolTipText","Anuluj");
-        UIManager.put("FileChooser.fileNameHeaderText","Nazwa pliku");
-        UIManager.put("FileChooser.upFolderToolTipText", "Poziom wyżej");
-        UIManager.put("FileChooser.homeFolderToolTipText","Pulpit");
-        UIManager.put("FileChooser.newFolderToolTipText","Stwórz nowy folder");
-        UIManager.put("FileChooser.listViewButtonToolTipText","Lista");
-        UIManager.put("FileChooser.newFolderButtonText","Stwórz nowy folder");
-        UIManager.put("FileChooser.renameFileButtonText", "Zmień nazwę pliku");
-        UIManager.put("FileChooser.deleteFileButtonText", "Usuń plik");
-        UIManager.put("FileChooser.filterLabelText", "Typy plików");
-        UIManager.put("FileChooser.detailsViewButtonToolTipText", "Detale");
-        UIManager.put("FileChooser.fileSizeHeaderText","Rozmiar");
-        UIManager.put("FileChooser.fileDateHeaderText", "Data modyfikacji");
-       
-        UIManager.put("OptionPane.yesButtonText", "Tak");
-        UIManager.put("OptionPane.noButtonText", "Nie");
-        UIManager.put("OptionPane.cancelButtonText", "Anuluj");
-        UIManager.put("OptionPane.titleText", "Tytuł");
-    }
+
+    private static File toOpen = null;
 }
--- src/st/finanse/UpdateI.java
+++
@@ -1,9 +0,0 @@
-package st.finanse;
-
-/**
- * Interfejs umożliwiający aktualizowanie wszystkich okien.
- * @author Norbert "ShookTea" Kowalik
- */
-public interface UpdateI {
-    public void updateData();
-}
