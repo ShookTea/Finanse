c081e2ba3af83810d2030ec9108e6d39dc1b7e2f
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-03 16:27:30 +0100

    Uniwersalny mechanizm używania formatów

--- src/st/finanse/Project.java
+++ src/st/finanse/Project.java
@@ -3,8 +3,10 @@ package st.finanse;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import st.finanse.data.Month;
+import st.finanse.format.Format;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Project {
     private Project() {}
@@ -14,6 +16,15 @@ public class Project {
     public static Project PROJECT = new Project();
 
     public static void loadProject(File f) {
+        PROJECT = Format.loadProject(f);
+        if (PROJECT == null) createNewProject();
+    }
 
+    public static void saveProject(File f) throws IOException {
+        Format.saveProject(PROJECT, f);
+    }
+
+    public static void createNewProject() {
+        PROJECT = new Project();
     }
 }
--- src/st/finanse/format/Format.java
+++ src/st/finanse/format/Format.java
@@ -3,8 +3,20 @@ package st.finanse.format;
 import st.finanse.Project;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface Format {
     public Project loadFrom(File file);
     public void saveTo(Project project, File file);
+
+    public static Project loadProject(File f) {
+        return null;
+    }
+
+    public static void saveProject(Project project, File file) throws IOException {
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        
+    }
 }
