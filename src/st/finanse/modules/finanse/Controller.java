41f524a7fad14029a8c6e8b9b475fd4d89f5a492
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-03 18:30:07 +0100

    Tworzenie drzewa miesięcy

--- src/st/finanse/modules/finanse/Controller.java
+++ src/st/finanse/modules/finanse/Controller.java
@@ -1,53 +1,75 @@
 package st.finanse.modules.finanse;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import st.finanse.Project;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Controller {
-
-    @FXML private TreeView<?> monthTree;
-
+    @FXML private TreeView<String> monthTree;
     @FXML private Label monthTitle;
-
     @FXML private TableView<?> table;
-
     @FXML private Spinner<?> entryDay;
-
     @FXML private CheckBox isHoliday;
-
     @FXML private TextField entryTitle;
-
     @FXML private TextField entryAmount;
-
     @FXML private Button entryAccepted;
+    private ObjectProperty<MonthEntry> currentEntry = new SimpleObjectProperty<>();
 
     @FXML
-    void addEntry(ActionEvent event) {
+    private void addEntry(ActionEvent event) {
 
     }
 
     @FXML
-    void monthChoosen(MouseEvent event) {
+    private void monthChoosen(MouseEvent event) {
 
     }
 
     @FXML
-    void tableClicked(MouseEvent event) {
+    private void tableClicked(MouseEvent event) {
 
     }
 
     @FXML
-    void initialize() {
+    private void initialize() {
+        reloadTree();
+    }
 
+    private void reloadTree() {
+        TreeItem<String> root = new TreeItem<>();
+        root.setExpanded(true);
+        Map<Integer, List<MonthEntry>> map = new HashMap<>();
+        for (MonthEntry me : Project.PROJECT.FINANSE_MONTHS) {
+            int year = me.month.getYear();
+            if (!map.containsKey(year)) map.put(year, new ArrayList<>());
+            map.get(year).add(me);
+        }
+        for (Integer year : map.keySet()) {
+            System.out.println("year " + year);
+            TreeItem<String> yearItem = new TreeItem<>("Rok " + year);
+            for (MonthEntry me : map.get(year)) {
+                yearItem.getChildren().add(new MonthTreeItem(me));
+            }
+            root.getChildren().add(yearItem);
+        }
+        monthTree.setRoot(root);
+    }
+
+    private class MonthTreeItem extends TreeItem<String> {
+        public MonthTreeItem(MonthEntry me) {
+            super(me.month.getMonthName());
+            this.monthEntry = me;
+        }
+
+        public final MonthEntry monthEntry;
     }
 }
