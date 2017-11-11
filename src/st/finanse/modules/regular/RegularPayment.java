7bbafcae705065df2eebc65e90ebde203a9aca7d
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 15:19:51 +0100

    Wstęp do modułu opłat regularnych

---
+++ src/st/finanse/modules/regular/RegularPayment.java
@@ -0,0 +1,10 @@
+package st.finanse.modules.regular;
+
+public class RegularPayment {
+    public RegularPayment(String name) {
+        this.name = name;
+    }
+
+    
+    public final String name;
+}
