77a1dee9b80874261402fe54388565837ceed7f2
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 16:54:28 +0100

    Tworzenie nowych płatności regularnych

--- src/st/finanse/modules/regular/Controller.java
+++ src/st/finanse/modules/regular/Controller.java
@@ -2,6 +2,7 @@ package st.finanse.modules.regular;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import st.finanse.Project;
 import st.finanse.gui.MainWindowController;
 import st.finanse.gui.Updateable;
 
@@ -29,11 +30,29 @@ public class Controller implements Updateable {
 
     @FXML
     private void createRegularPayment() {
+        TextInputDialog tid = new TextInputDialog("Rachunek za prąd");
+        tid.setTitle("Dodawanie nowej płatności regularnej");
+        tid.setHeaderText("Płatność regularna");
+        tid.setContentText("Nazwa płatności:");
+        tid.showAndWait().ifPresent(name -> {
+            RegularPayment rp = new RegularPayment(name);
+            Project.PROJECT.REGULAR_PAYMENTS.addAll(rp);
+            updateList();
+        });
+    }
+
+    @FXML
+    private void selectPayment() {
 
     }
 
     @Override
     public void update() {
+        updateList();
+    }
 
+    private void updateList() {
+        regularList.getItems().clear();
+        Project.PROJECT.REGULAR_PAYMENTS.stream().forEach(r -> regularList.getItems().add(r.name));
     }
 }
