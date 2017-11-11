576a6fbbacf88244f17b0d5faf6d1229fa44df28
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 16:23:42 +0100

    Utworzenie widoku płatności regularnych

---
+++ src/st/finanse/modules/regular/Controller.java
@@ -0,0 +1,38 @@
+package st.finanse.modules.regular;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import st.finanse.gui.MainWindowController;
+import st.finanse.gui.Updateable;
+
+public class Controller implements Updateable {
+
+    @FXML private ListView<String> regularList;
+    @FXML private TableView<String> entryTable;
+
+    @FXML private DatePicker entryDate;
+    @FXML private TextField amount;
+    @FXML private CheckBox isPayed;
+    @FXML private DatePicker paymentDate;
+
+    @FXML
+    private void initialize() {
+        MainWindowController.UPDATEABLES.add(this);
+        update();
+    }
+
+    @FXML
+    private void createEntry() {
+
+    }
+
+    @FXML
+    private void createRegularPayment() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
