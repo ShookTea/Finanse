7981a69d6295663a191955401fe357ed35b0dcee
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 13:11:56 +0100

    Podstawy pod wyświetlanie wykresów

---
+++ src/st/finanse/modules/summary/Controller.java
@@ -0,0 +1,13 @@
+package st.finanse.modules.summary;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
+public class Controller {
+    @FXML private LineChart lineChart;
+
+    @FXML
+    private void initialize() {
+
+    }
+}
