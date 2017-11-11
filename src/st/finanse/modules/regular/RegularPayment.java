697b82a6f86a9f57edfa1996d38be5c155f4d5ab
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 15:32:26 +0100

    Tworzenie kompletu płatności regularnych

--- src/st/finanse/modules/regular/RegularPayment.java
+++ src/st/finanse/modules/regular/RegularPayment.java
@@ -1,10 +1,13 @@
 package st.finanse.modules.regular;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RegularPayment {
     public RegularPayment(String name) {
         this.name = name;
     }
 
-    
+    public final List<PaymentEntry> payments = new ArrayList<>();
     public final String name;
 }
