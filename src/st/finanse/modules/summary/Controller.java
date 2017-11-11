04f41b73c9f1d0be2b0cf47badff2afcda68e293
Norbert Kowalik <kowaliknor@gmail.com>
2017-11-11 14:15:43 +0100

    Poprawki

--- src/st/finanse/modules/summary/Controller.java
+++ src/st/finanse/modules/summary/Controller.java
@@ -5,11 +5,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart.Series;
 import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import st.finanse.Project;
 import st.finanse.data.Amount;
-import st.finanse.data.Month;
 import st.finanse.gui.MainWindowController;
 import st.finanse.gui.Updateable;
 import st.finanse.modules.finanse.MonthEntry;
@@ -26,12 +25,12 @@ public class Controller implements Updateable {
 
     @Override
     public void update() {
-        accountBilanse.setData(createBilanseData());
-        gainsLoss.setData(createGainsLossData());
+        createBilanseData();
+        createGainsLossData();
         gainsLoss.setBarGap(1.0);
     }
 
-    private ObservableList<Series<String, Double>> createBilanseData() {
+    private void createBilanseData() {
         ObservableList<Series<String, Double>> result = FXCollections.observableArrayList();
         Series<String, Double> bilanse = new Series<>();
         bilanse.setName("Stan konta na koniec miesiąca");
@@ -39,10 +38,10 @@ public class Controller implements Updateable {
             bilanse.getData().add(new Data(monthEntry.month.toString(), monthEntry.getCurrentAmount().toDouble()));
         }
         result.addAll(bilanse);
-        return result;
+        accountBilanse.setData(result);
     }
 
-    private ObservableList<Series<String,Double>> createGainsLossData() {
+    private void createGainsLossData() {
         ObservableList<Series<String, Double>> result = FXCollections.observableArrayList();
         Series<String, Double> gains = new Series<>();
         gains.setName("Przychody");
@@ -60,6 +59,6 @@ public class Controller implements Updateable {
             bilanse.getData().add(new Data(monthName, diff.toDouble()));
         }
         result.addAll(gains, losses, bilanse);
-        return result;
+        gainsLoss.setData(result);
     }
 }
